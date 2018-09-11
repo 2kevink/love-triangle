8ad2d04a421588670e89c93311aeb4922535f767
2kevink <kevink2@yandex.ru>
2018-09-11 03:03:31 +0300

    '2'

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for(var i = 0; i<preferences.length; i++){
+    if(preferences[preferences[preferences[i]-1]-1]-1==i ) count ++;
+    if(preferences[i]-1 == i) count --;
+  }
+  console.log(count);
+  return count/3;
 };
